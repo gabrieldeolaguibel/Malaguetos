0694e5ff4260cbedc05c80b5808f1970492be11b
Lukasaurus11 <lucacuneob@gmail.com>
2023-12-12 16:56:11 +0100

    Created new tests
    Created collisions test and updated readme

--- src/googletest.cpp
+++ src/googletest.cpp
@@ -1,26 +1,43 @@
 #include <gtest/gtest.h>
 #include "game.h"
 
-TEST(Run_test, run) {
+TEST(RunTest, Run) {
     Game game;
     EXPECT_EQ(game.run(), 0);
 }
 
-TEST(Position_test, innit_snake_position) {
+TEST(PositionTest, InitSnakePosition) {
     Game game;
     EXPECT_EQ(game.getSnakePosition(0).x, 400.0f);
     EXPECT_EQ(game.getSnakePosition(0).y, 300.0f);
 }
 
-TEST(Size_Test, increaseSize) {
+TEST(SizeTest, ChangeSize) {
     Game game;
     game.growSnake(1);
     EXPECT_EQ(game.getSnakeSize(), 2);
-}
 
-TEST(Size_Test, decreaseSize) {
-    Game game;
-    game.growSnake(1);
     game.shrinkSnake(1);
     EXPECT_EQ(game.getSnakeSize(), 1);
+}
+
+
+TEST(GameTest, CollisionDetection) {
+    // Create an instance of the Game class
+    Game game;
+
+    // Scenario 1: Test collision between snake head and apple (expected collision)
+    Position snakeHead(100, 100); // Set snake head position
+    sf::CircleShape apple;
+    apple.setPosition(120, 120); // Set apple position
+    apple.setRadius(20); // Set apple radius
+
+    // Expect a collision between snake head and apple
+    EXPECT_TRUE(game.checkCollision(snakeHead, apple));
+
+    // Scenario 2: Test collision between snake head and apple (expected no collision)
+    apple.setPosition(250, 250); // Set a position far from the snake head
+
+    // Expect no collision between snake head and apple
+    EXPECT_FALSE(game.checkCollision(snakeHead, apple));
 }
