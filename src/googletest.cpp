39cce52e2030969810be0cb8c85f2eccc0bd12f2
Lukasaurus11 <lucacuneob@gmail.com>
2023-12-12 11:14:32 +0100

    Updated doc

--- src/googletest.cpp
+++ src/googletest.cpp
@@ -1,10 +1,10 @@
-#include <gtest/gtest.h>
-
-TEST(ExampleTest, Addition) {
-    EXPECT_EQ(1 + 1, 2);
-}
-
-int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
+//#include <gtest/gtest.h>
+//
+//TEST(ExampleTest, Addition) {
+//    EXPECT_EQ(1 + 1, 2);
+//}
+//
+//int main(int argc, char** argv) {
+//    ::testing::InitGoogleTest(&argc, argv);
+//    return RUN_ALL_TESTS();
+//}
